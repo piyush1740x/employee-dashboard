eca0c1978fecbd47471b76d302b62d05cd9bc864
Harsh chaudhary <146811538+piyush1740x@users.noreply.github.com>
2025-03-20 01:45:32 +0530

    final

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,17 @@
 "use client"
 import EmpCard from '@/components/EmpCard'
-import Model from '@/components/Model'
 import SearchBaar from '@/components/SearchBaar'
 import React from 'react'
 import { useState } from 'react'
 
-const page = () => {
+const Page = () => {
   const [searchQuery, setSearchQuery] = useState("");
   return (
     <div className='p-6 relative'>
-      {/* <Model/> */}
       <SearchBaar setSearchQuery={setSearchQuery}/>
       <EmpCard searchQuery={searchQuery}/>
     </div>
   )
 }
 
-export default page
+export default Page
